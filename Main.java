716f5ad7f25ba26bbe6a598025e10638e2b8fd53
Ameya2137 <164353861+Ameya2137@users.noreply.github.com>
2025-02-01 18:04:46 +0530

    Added Main.java with program entry point

---
+++ Main.java
@@ -0,0 +1,28 @@
+// Name: Ameya Tipnis
+// PRN: 23070126012
+// Batch: SY-AIML-A1
+
+// Main.java
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		//Creating a Scanner object to read user input
+		Scanner scanner = new Scanner(System.in);
+        		UserInput userInput = new UserInput(scanner);
+        		Calculator calculator = new Calculator();
+
+		// Infinite loop to repeatedly prompt the user for input
+        		while (true) {
+			// To get user's choice of operation
+            			int choice = userInput.getChoice();
+            			if (choice == 10) {
+                				System.out.println("Exiting...");
+                				break;
+            			}
+            			userInput.performCalculation(choice, calculator);
+        		}
+		// Closing the scanner object inorder to prevent resource leaks
+        		scanner.close();
+    	}
+}
